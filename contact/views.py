e99e1255b174545d24473146be098b45cfdabcf1
Nikita Hodovanyj <nikitahodovanyj@MacBook-Pro-Nikita.local>
2025-02-08 02:12:06 +0200

    doing my_crqode and standard subscribe

--- contact/views.py
+++ contact/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def render_contact(request):
-    return render(request = request, template_name = "contact/contact.html")
+    return render(request = request, template_name = "contact/contact.html", context={'footer': True})
--- my_qrcodes/views.py
+++ my_qrcodes/views.py
@@ -1,22 +1,12 @@
 
 from django.shortcuts import render
-# from create.models import Qrcode
-from django.contrib.auth.decorators import login_required
-
-
-
-
-
 
 
 def render_my_qrcodes(request):
-    # Получаем только QR-коды текущего пользователя
-    print(request.user)  # Проверим, кто текущий пользователь
-
-    from create.models import Qrcode  # Импорт модел
+    from create.models import Qrcode 
     user_qrcodes = Qrcode.objects.filter(user=request.user)  
     return render(request, 'my_qrcodes/my_qrcodes.html', {
-        'user_qrcodes': user_qrcodes,  # Передаем в шаблон только свои QR-коды
+        'user_qrcodes': user_qrcodes,  
         'footer': True
     })
 
